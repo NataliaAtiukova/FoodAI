c6a83f01bb55dfe35ca8fbcf9a9a0a6cea09a7d8
NatalyaAtyukova <atukowa.natalya@yandex.ru>
2025-09-29 20:52:14 +0300

    chore: rename app to com.app.foodai

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 }
 
 android {
-    namespace = "com.example.food_ai"
-    compileSdk = flutter.compileSdkVersion
+    namespace = "com.app.foodai"
+    compileSdk = 35
     // Force NDK version to satisfy plugin requirements
     ndkVersion = "27.0.12077973"
 
@@ -22,11 +22,12 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.food_ai"
+        applicationId = "com.app.foodai"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 34
+        multiDexEnabled = true
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -42,4 +43,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation("androidx.multidex:multidex:2.0.1")
 }
--- android/app/src/main/kotlin/com/app/foodai/MainActivity.kt
+++ android/app/src/main/kotlin/com/app/foodai/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.food_ai
+package com.app.foodai
 
 import io.flutter.embedding.android.FlutterActivity
 
